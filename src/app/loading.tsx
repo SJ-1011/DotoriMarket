48cb3169d372be642361a300dc1a1e41d15ad63d
yebin-jeong <2022112390@dgu.ac.kr>
2025-07-14 21:55:12 +0900

    🎨feat: 로딩 페이지

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,25 +1,20 @@
 'use client';
 
-import { useEffect } from 'react';
 import Image from 'next/image';
 import Header from '@/components/common/Header';
 import Footer from '@/components/common/Footer';
 
-export default function Error({ error }: { error: Error }) {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
-
+export default function Loading() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
 
       <main className="flex-1 flex flex-col items-center justify-center">
-        <Image src="/error-dotori.png" alt="에러 도토리 이미지" width={200} height={200} priority />
+        <Image src="/loading-dotori.png" alt="로딩 도토리 이미지" width={400} height={400} priority />
         <h1 className="mt-10 text-center">
-          앗, 도토리가 길을 잃었어요🍂
+          곧 도토리섬에 도착해요!
           <br />
-          잠시 후 다시 시도해 주세요!
+          조금만 기다려 주세요🧺
         </h1>
       </main>
 
