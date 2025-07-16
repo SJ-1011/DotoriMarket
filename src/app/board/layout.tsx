f9d19c1f1afb17351374ea2f14b386440b40d934
noognoog <noognoog@naver.com>
2025-07-16 22:45:20 +0900

    💄 layout.tsx 헤더 주석처리 - 헤더 중복이슈로 layout.tsx에서 header  주석처리

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -5,12 +5,12 @@ import Link from 'next/link';
 export default function BoardPageLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <div className="flex flex-col-reverse sm:flex-row max-w-[1000px] mx-auto">
         {/* Content */}
         <main className="flex-1">{children}</main>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
