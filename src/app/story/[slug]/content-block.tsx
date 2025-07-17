987214f93b2ef42f37595c492b3218a60b962aca
yebin-jeong <2022112390@dgu.ac.kr>
2025-07-17 16:07:06 +0900

    ♻️refactor: 주석 수정

--- src/app/story/[slug]/content-block.tsx
+++ src/app/story/[slug]/content-block.tsx
@@ -5,9 +5,7 @@ import Image from 'next/image';
 import type { ContentBlock } from '@/types/story';
 
 export default function StoryBlock({ title, content, imageUrl }: ContentBlock) {
-  // content를 줄바꿈(\n) 기준으로 나누어 각 문단을 <p>로 감싸기
   const paragraphs = content.split('\n');
-
   return (
     <div className="w-full flex flex-col items-center mb-12">
       <div className="w-[50%] h-0.5 bg-black my-12" />
