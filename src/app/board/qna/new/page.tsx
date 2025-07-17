5e730d1fdd9a4a715f83464d6612c020e323d9f9
noognoog <noognoog@naver.com>
2025-07-17 15:34:38 +0900

    💄style: 문의게시판 글쓰기 페이지 디자인 수정

--- src/app/board/qna/new/page.tsx
+++ src/app/board/qna/new/page.tsx
@@ -5,8 +5,10 @@ export default async function NewQnaPage() {
 
   return (
     <main className="flex-1 min-w-[320px] p-4">
-      <div className="text-center py-4">
-        <h2 className="text-2xl font-bold text-gray-700 dark:text-gray-200">게시글 등록</h2>
+      <div className="py-4">
+        <div className="mb-3  text-xs sm:text-sm lg:text-base  text-gray-400">홈 &gt; 문의게시판 &gt; 문의하기</div>
+
+        <h2 className="text-center  text-2xl font-bold text-black ">문의하기</h2>
       </div>
       <section className="mb-8 p-4">
         <RegistForm boardType={boardType} />
