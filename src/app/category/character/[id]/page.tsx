2c2b0fbdb4079dc07466091d1960c6caa0929b8a
Seonjin <ebngk0@gmail.com>
2025-07-22 14:06:58 +0900

    🐛 fix: params 버그 수정

--- src/app/category/character/[id]/page.tsx
+++ src/app/category/character/[id]/page.tsx
@@ -1,19 +1,20 @@
 import { CHARACTER_CATEGORIES } from '@/constants/categories';
 import CategoryPage from '../../CategoryPage';
 
-export default function CharacterPage({ params }: { params: { id: string } }) {
+export default async function CharacterPage({ params }: { params: { id: string } }) {
   const detailArray = CHARACTER_CATEGORIES.map((name, index) => ({
     name,
     address: `/character/${(index + 1).toString().padStart(2, '0')}`,
   }));
 
+  const { id } = await params;
   // 페이지 넘버
-  const pageNumber = Number(params.id) - 1;
+  const pageNumber = Number(id) - 1;
   // 페이지 제목 ex. 스튜디오 지브리, 핑구 등...
   const title = CHARACTER_CATEGORIES[pageNumber];
   return (
     <>
-      <CategoryPage category="character" title={title} detail={params.id} categoryName="캐릭터" detailArray={detailArray} />
+      <CategoryPage category="character" title={title} detail={id} categoryName="캐릭터" detailArray={detailArray} />
     </>
   );
 }
--- src/app/category/stationery/[id]/page.tsx
+++ src/app/category/stationery/[id]/page.tsx
@@ -1,19 +1,20 @@
 import { STATIONERY_CATEGORIES } from '@/constants/categories';
 import CategoryPage from '../../CategoryPage';
 
-export default function StationeryPage({ params }: { params: { id: string } }) {
+export default async function StationeryPage({ params }: { params: { id: string } }) {
   const detailArray = STATIONERY_CATEGORIES.map((name, index) => ({
     name,
     address: `/stationery/${(index + 1).toString().padStart(2, '0')}`,
   }));
 
+  const { id } = await params;
   // 페이지 넘버
-  const pageNumber = Number(params.id) - 1;
+  const pageNumber = Number(id) - 1;
   // 페이지 제목 ex. 스튜디오 지브리, 핑구 등...
   const title = STATIONERY_CATEGORIES[pageNumber];
   return (
     <>
-      <CategoryPage category="stationery" title={title} detail={params.id} categoryName="문구" detailArray={detailArray} />
+      <CategoryPage category="stationery" title={title} detail={id} categoryName="문구" detailArray={detailArray} />
     </>
   );
 }
