dddfa405f39ea933d6df950ef76cbe7b73e5a98a
yebin-jeong <2022112390@dgu.ac.kr>
2025-07-23 15:37:24 +0900

    🎨 feat: 탭섹션 구현

--- src/app/products/[id]/TabSection.tsx
+++ src/app/products/[id]/TabSection.tsx
@@ -5,23 +5,26 @@ import ProductReviews from './Reviews';
 import ProductQuestions from './Questions';
 
 export default function ProductTabSection({ product, activeTab, setActiveTab }: { product: Product; activeTab: string; setActiveTab: (tab: string) => void }) {
-  const tabs = ['상품정보', '쇼핑가이드', '상품후기', '상품문의'];
+  const tabs = ['상품 정보', '쇼핑 가이드', '상품 후기', '상품 문의'];
 
   return (
     <>
-      <nav className="flex space-x-4 border-b mb-4">
-        {tabs.map(tab => (
-          <button key={tab} onClick={() => setActiveTab(tab)} className={`px-4 py-2 ${activeTab === tab ? 'border-b-2 border-black font-semibold' : 'text-gray-500'}`}>
-            {tab}
-          </button>
-        ))}
+      <nav className="flex my-12 mb-4 bg-primary-light overflow-hidden" style={{ height: '44px' }}>
+        {tabs.map(tab => {
+          const isActive = activeTab === tab;
+          return (
+            <button key={tab} onClick={() => setActiveTab(tab)} className={`flex-1 text-center font-medium text-sm cursor-pointer transition ${isActive ? 'bg-background' : 'text-black bg-transparent'} `} style={{ lineHeight: '44px' }}>
+              {tab}
+            </button>
+          );
+        })}
       </nav>
 
       <div>
-        {activeTab === '상품정보' && <ProductInfo content={product.content} />}
-        {activeTab === '쇼핑가이드' && <ShoppingGuide />}
-        {activeTab === '상품후기' && <ProductReviews productId={product._id} />}
-        {activeTab === '상품문의' && <ProductQuestions productId={product._id} />}
+        {activeTab === '상품 정보' && <ProductInfo content={product.content} />}
+        {activeTab === '쇼핑 가이드' && <ShoppingGuide />}
+        {activeTab === '상품 후기' && <ProductReviews productId={product._id} />}
+        {activeTab === '상품 문의' && <ProductQuestions productId={product._id} />}
       </div>
     </>
   );
