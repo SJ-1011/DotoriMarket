780a7b6b44511eac1c445bbf340c71b109cdb2c3
noognoog <noognoog@naver.com>
2025-07-25 10:01:11 +0900

    🐛 fix: params 비동기 처리 로직 추가

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,12 +1,9 @@
 import { getProductById } from '@/utils/getProducts';
 import ProductDetailPage from '@/app/products/[id]/DetailPage';
 
-interface Props {
-  params: { id: string };
-}
-
-export default async function ProductPage({ params }: Props) {
-  const res = await getProductById(Number(params.id));
+export default async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const res = await getProductById(Number(id));
 
   if (!res.ok || !res.item) {
     return <div>상품을 불러올 수 없습니다.</div>;
