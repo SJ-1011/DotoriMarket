4f516bc1b8fe371958db8adfb69498b9b58ac7ce
YouVin <dbqls990909@naver.com>
2025-07-26 11:47:56 +0900

    🎨 feaft: 제출폼 제작

---
+++ src/app/order/OrderClient.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useFormContext } from 'react-hook-form';
+import type { OrderForm } from '@/types/Order';
+import type { CartItem } from '@/types/Cart';
+import OrderUserInfo from './OrderUserInfo';
+import OrderProductList from './OrderProductList';
+import OrderCostSummary from './OrderCostSummary';
+
+interface Props {
+  cartItems: CartItem[];
+  cartCost: {
+    products: number;
+    shippingFees: number;
+    discount: { products: number; shippingFees: number };
+    total: number;
+  };
+  userInfo: {
+    name: string;
+    recipient: string;
+    phone: string;
+    address: string;
+  };
+}
+
+export default function OrderClient({ cartCost, cartItems, userInfo }: Props) {
+  // context에서 받아옴
+  const { handleSubmit } = useFormContext<OrderForm>();
+
+  const onSubmit = (data: OrderForm) => {
+    console.log('최종 API 전송 데이터:', data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <OrderUserInfo name={userInfo.name} recipient={userInfo.recipient} phone={userInfo.phone} address={userInfo.address} />
+      <OrderProductList items={cartItems} />
+      <OrderCostSummary cartCost={cartCost} />
+      <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded">
+        주문하기
+      </button>
+    </form>
+  );
+}
