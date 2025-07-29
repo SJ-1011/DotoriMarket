27c62d1aaf9b4c3598b7e9db1124ffda5bdd6057
Seonjin <ebngk0@gmail.com>
2025-07-29 15:27:59 +0900

    💄 style: UI 일부 변경

--- src/app/mypage/OrderHistory.tsx
+++ src/app/mypage/OrderHistory.tsx
@@ -39,7 +39,7 @@ export default function OrderHistory() {
   const createdDay: string[] = [];
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }, []);
 
   useEffect(() => {
