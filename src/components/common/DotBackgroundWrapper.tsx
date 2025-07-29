c4c27b1a19129cdb9262809c7ea8ab6caa21f5d3
yebin-jeong <2022112390@dgu.ac.kr>
2025-07-29 17:36:33 +0900

    💄style: 메인페이지 모바일 레이아웃 재정의

--- src/components/common/DotBackgroundWrapper.tsx
+++ src/components/common/DotBackgroundWrapper.tsx
@@ -3,5 +3,9 @@
 import React from 'react';
 
 export default function DotBackgroundWrapper({ children }: { children: React.ReactNode }) {
-  return <div className="dot-bg-wrapper">{children}</div>;
+  return (
+    <div className="dot-bg-wrapper">
+      <div className="dot-bg-inner">{children}</div>
+    </div>
+  );
 }
