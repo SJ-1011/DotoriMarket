503b3279b1775eebad880b6a9676b13ca5b61b3f
Seonjin <ebngk0@gmail.com>
2025-07-30 20:27:10 +0900

    💄 style: 상품 카드에 새로운 UI 추가

--- src/components/common/ProductItemCard.tsx
+++ src/components/common/ProductItemCard.tsx
@@ -1,5 +1,6 @@
 import { Product } from '@/types/Product';
 import ProductCard from './ProductCard';
+import ProductCardLarge from './ProductCardLarge';
 
 interface LikedProduct extends Product {
   bookmarkId: number;
@@ -8,17 +9,34 @@ interface LikedProduct extends Product {
 interface ProductItemCardProps {
   products: Product[] | null;
   likedProducts?: LikedProduct[] | null;
+  type?: 'large' | null;
 }
 
-export default function ProductItemCard({ products, likedProducts }: ProductItemCardProps) {
-  return (
-    <>
-      {products &&
-        products.map(product => {
-          const liked = likedProducts?.find(likedProduct => likedProduct._id === product._id);
-          const bookmarkId = liked ? liked.bookmarkId : 0;
-          return <ProductCard key={product._id} product={product} bookmarkId={bookmarkId} />;
-        })}
-    </>
-  );
+export default function ProductItemCard({ products, likedProducts, type }: ProductItemCardProps) {
+  if (type === 'large') {
+    return (
+      <>
+        {products &&
+          products.map((product, index) => {
+            const liked = likedProducts?.find(likedProduct => likedProduct._id === product._id);
+            const bookmarkId = liked ? liked.bookmarkId : 0;
+            return (
+              <li key={product._id}>
+                <ProductCardLarge product={product} bookmarkId={bookmarkId} index={index + 1} />
+              </li>
+            );
+          })}
+      </>
+    );
+  } else
+    return (
+      <>
+        {products &&
+          products.map(product => {
+            const liked = likedProducts?.find(likedProduct => likedProduct._id === product._id);
+            const bookmarkId = liked ? liked.bookmarkId : 0;
+            return <ProductCard key={product._id} product={product} bookmarkId={bookmarkId} />;
+          })}
+      </>
+    );
 }
