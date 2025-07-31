f18c872a806a7bb8ee1553c18a31e99070f9dfb5
YouVin <dbqls990909@naver.com>
2025-07-31 14:05:59 +0900

    🐛 fix: 모바일 구매 버튼에 배경 넣기

--- src/app/cart/CartButtons.tsx
+++ src/app/cart/CartButtons.tsx
@@ -8,12 +8,12 @@ export default function CartButtons({ handleDeleteSelected }: CartButtonsProps) 
   return (
     <div className="flex justify-between items-center py-2 sm:mt-4 lg:mt-8">
       {/* 선택상품 삭제 버튼 */}
-      <button onClick={handleDeleteSelected} className="border border-black px-2 py-1 sm:px-4 sm:py-2 text-sm sm:text-base hover:bg-black hover:text-white transition-colors cursor-pointer">
+      <button onClick={handleDeleteSelected} className="border border-dark-gray px-2 py-1 sm:px-4 sm:py-2 text-sm sm:text-sm lg:text-base hover:bg-dark-gray hover:text-white transition-colors cursor-pointer">
         선택상품 삭제
       </button>
 
       {/* 안내문 */}
-      <span className="hidden sm:block text-xs sm:text-sm text-gray-600">장바구니는 최대 10개의 상품을 담을 수 있습니다.</span>
+      <span className="hidden sm:block text-xs sm:text-sm text-gray">장바구니는 최대 10개의 상품을 담을 수 있습니다.</span>
     </div>
   );
 }
