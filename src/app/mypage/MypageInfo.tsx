e1b617e218000c0affe745ed5f38f0c79d3f147c
noognoog <noognoog@naver.com>
2025-07-31 09:47:07 +0900

    🎨 feat: 라우팅 경로 수정

--- src/app/mypage/MypageInfo.tsx
+++ src/app/mypage/MypageInfo.tsx
@@ -105,11 +105,11 @@ export default function MypageInfo() {
               </Link>
             </li>
             <li className="flex flex-col flex-nowrap justify-center gap-2 items-center w-[6rem] lg:w-[8rem]">
-              <Link href="/mypage/admin/posts" className="flex flex-col flex-nowrap text-xs lg:text-sm sm:w-[6rem] lg:w-[8rem] shadow-[0_0_5px_rgba(0,0,0,0.1)] aspect-square  p-2 sm:p-4 bg-white rounded-2xl sm:rounded-4xl justify-center items-center sm:gap-1 lg:gap-4">
+              <Link href="/mypage/admin/stats" className="flex flex-col flex-nowrap text-xs lg:text-sm sm:w-[6rem] lg:w-[8rem] shadow-[0_0_5px_rgba(0,0,0,0.1)] aspect-square  p-2 sm:p-4 bg-white rounded-2xl sm:rounded-4xl justify-center items-center sm:gap-1 lg:gap-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-10">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                 </svg>
-                <p>게시글 관리</p>
+                <p>판매 실적 조회</p>
               </Link>
             </li>
           </ul>
