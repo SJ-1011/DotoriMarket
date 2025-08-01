f9a6253169c7847a6760397d8cecb2b288977a82
yebin-jeong <2022112390@dgu.ac.kr>
2025-08-02 00:29:36 +0900

    ♻️ refactor: getFullImageUrl 함수 null일 경우 추가

--- src/utils/getFullImageUrl.ts
+++ src/utils/getFullImageUrl.ts
@@ -1,6 +1,7 @@
 const baseUrl = process.env.NEXT_PUBLIC_API_URL ?? 'https://fesp-api.koyeb.app/market';
 
-export const getFullImageUrl = (imagePath: string): string => {
+export const getFullImageUrl = (imagePath?: string): string | null => {
+  if (!imagePath) return null;
   if (imagePath.startsWith('http')) {
     return imagePath;
   }
