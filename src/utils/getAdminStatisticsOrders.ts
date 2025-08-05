fa8126542ff7ef31b0acca406b16a64b41e725dd
Seonjin <ebngk0@gmail.com>
2025-08-05 14:00:59 +0900

    🐛 fix: 통계 함수 오류 수정

--- src/utils/getAdminStatisticsOrders.ts
+++ src/utils/getAdminStatisticsOrders.ts
@@ -3,6 +3,7 @@ import type { ApiResPromise } from '@/types/api';
 interface Statistics {
   totalQuantity: number;
   totalSales: number;
+  date: string;
 }
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -11,9 +12,25 @@ const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
 /**
  * 로그인한 사용자의 알림 목록을 가져옵니다.
  */
-export async function getAdminStatisticsOrders(token: string): ApiResPromise<Statistics> {
+export async function getAdminStatisticsOrders(token: string): ApiResPromise<Statistics[]> {
+  // 🔹 날짜 포맷 함수
+  const formatDateToDot = (date: Date): string => `${date.getFullYear()}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getDate().toString().padStart(2, '0')}`;
+
+  // 🔹 8/1 ~ 오늘 날짜 설정
+  const startDate = new Date();
+  startDate.setMonth(7); // 8월 (month는 0부터 시작)
+  startDate.setDate(1); // 1일
+
+  const today = new Date();
+
+  const searchParams = new URLSearchParams({
+    start: formatDateToDot(startDate), // "2025.08.01"
+    finish: formatDateToDot(today), // 예: "2025.08.05"
+    by: 'day',
+  });
+
   try {
-    const requests = await fetch(`${API_URL}/admin/statistics/orders`, {
+    const requests = await fetch(`${API_URL}/admin/statistics/orders?${searchParams.toString()}`, {
       headers: {
         'Client-Id': CLIENT_ID,
         Authorization: `Bearer ${token}`,
