da263a55c2859aa56e9db187f3a2c4c22c15217a
YouVin <dbqls990909@naver.com>
2025-08-05 09:25:37 +0900

    💄 style: pointer-cursor 추가

--- src/app/order/complete/[id]/OrderCompleteButtons.tsx
+++ src/app/order/complete/[id]/OrderCompleteButtons.tsx
@@ -11,7 +11,7 @@ export default function OrderCompleteButtons() {
         onClick={() => router.push('/mypage')}
         className="w-1/2 py-2 sm:py-3
                    bg-primary text-white rounded-md sm:rounded-lg hover:bg-primary-dark
-                   text-sm sm:text-base lg:text-lg transition"
+                   text-sm sm:text-base lg:text-lg transition cursor-pointer"
       >
         주문 목록 가기
       </button>
@@ -20,7 +20,7 @@ export default function OrderCompleteButtons() {
         onClick={() => router.push('/')}
         className="w-1/2  py-2 sm:py-3 
                    bg-secondary-green text-white rounded-md sm:rounded-lg hover:bg-green-700
-                   text-xs sm:text-sm lg:text-base transition"
+                   text-xs sm:text-sm lg:text-base transition cursor-pointer"
       >
         쇼핑하러 가기
       </button>
